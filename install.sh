a1bbba65a5c33a700093638bf0f89ccd09a6a4b9
Andre Boyce <mobile@andreboyce.com>
2021-03-05 05:56:26 -0400

    update

--- install.sh
+++ install.sh
@@ -1,33 +1,6 @@
 #!/bin/bash
 
-install_node()
-{
-   echo "install node";
-   node_file="nodejs.tar.gz";
-   cd ~
-   mkdir bin
-   cd bin
-   rm -f node  
-   rm -f npm
-   rm -rf nodejs
-   wget -r -nd -l1 --no-parent -e robots=off -A "*linux-x64.tar.gz" https://nodejs.org/dist/latest/
-   mv node*linux-x64.tar.gz $node_file
-   tar -xvf $node_file;
-   rm -f ./$node_file;
-   mv node*linux-x64 nodejs
-   cp ./nodejs/bin/node ~/bin;
-   ln -s ./nodejs/lib/node_modules/npm/bin/npm-cli.js npm;
-   add_to_path $(pwd);
-   node --version;
-   npm --version;
-}
-
-if [ -x "$(command -v node)" ]; then
-  echo "Node found";
-else
-  install_node;
-fi
-
+# check if user is root
 if [[ $EUID -ne 0 ]]; 
    then
       # user is not root
@@ -36,6 +9,64 @@ if [[ $EUID -ne 0 ]];
       # user is root
       echo "This script must not be run as root";
       exit 1;
+fi
+
+# add directory to path
+add_to_path()
+{
+    if [[ "$PATH" =~ (^|:)"${1}"(:|$) ]]
+    then
+        return 0
+    fi
+    export PATH=${1}:$PATH
+}
+
+# install nodejs and npm
+install_node()
+{
+   echo "installing node";
+   node_file="nodejs.tar.gz";
+   cd ~;
+   mkdir bin;
+   cd bin;
+   rm -f node;
+   rm -f npm;
+   rm -rf nodejs;
+
+   # check for wget
+   if [ -x "$(command -v wget)" ]; then
+     #echo "wget found";
+     :
+   else
+     #
+     echo "wget not found";
+     exit 1;
+   fi
+
+   wget -r -nd -l1 --no-parent -e robots=off -A "*linux-x64.tar.gz" https://nodejs.org/dist/latest/;
+   mv node*linux-x64.tar.gz $node_file;
+   tar -xvf $node_file;
+   rm -f ./$node_file;
+   mv node*linux-x64 nodejs;
+   cp ./nodejs/bin/node ~/bin;
+   ln -s ./nodejs/lib/node_modules/npm/bin/npm-cli.js npm;
+   add_to_path $(pwd);
+   node --version;
+   npm --version;
+}
+
+echo "Checking for NodeJS";
+if [ -x "$(command -v node)" ]; then
+  echo "node found";
+else
+  install_node;
+fi
+
+echo "Checking for npm";
+if [ -x "$(command -v npm)" ]; then
+  echo "npm found";
+else
+  install_node;
 fi
 
 PORT=3000;
@@ -58,7 +89,7 @@ mysql -u $MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DB <  src/mysql/"${MYSQL_DB}.sql";
 
 mv src/server/RENAME_TO.env src/server/.env
 
-mv src/server/RENAME_TO.env src/client/.env
+mv src/client/RENAME_TO.env src/client/.env
 
 echo "Run ./start.sh to start";
 echo "Run ./stop.sh to stop";
