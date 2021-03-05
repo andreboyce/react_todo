e504caa7f3080cd71f29ef05d4234da661a61977
Andre Boyce <mobile@andreboyce.com>
2021-03-05 19:37:58 -0400

    update

--- src/restart.sh
+++ src/restart.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
 
-pkill $RTODO_SERVER_PID
+full_path=$(realpath $0);
+dir_path=$(dirname $full_path);
 
-pkill $RTODO_CLIENT_PID
-
-# change working directory to the directory the script exists in which should be rtodo/src
-cd "$(dirname "$0")";
-
-cd server;
-node app.js &
-RTODO_SERVER_PID=$!;
-echo "RTODO_SERVER_PID: $RTODO_SERVER_PID";
-
-cd "$(dirname "$0")";
-
-(cd src/client && exec npm start) &
-RTODO_CLIENT_PID=$!;
-echo "RTODO_CLIENT_PID: $RTODO_CLIENT_PID";
-cd "$(dirname "$0")";
+echo "$dir_path";
+"$dir_path/stop.sh";
+"$dir_path/start.sh";
